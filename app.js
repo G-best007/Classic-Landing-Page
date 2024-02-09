736935fb2af64b9f1d6dc0a44291a261c0bf97ce
Awotanmi Gbemisola <awotanmigbemisola.com>
2024-02-09 07:30:40 +0100

    few changes made

--- app.js
+++ app.js
@@ -6,3 +6,24 @@ const newYear = new Date().getFullYear()
 Year.innerHTML = newYear
 
 
+const mailForm = document.querySelector('.mailForm')
+const Subscribemail = document.querySelector('.Subscribemail')
+const buttonAddon = document.getElementById('button-addon2')
+
+mailForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const NewsubMail = Subscribemail.value
+     
+ if (NewsubMail === '' || null) {
+  Subscribemail.style.border = '2px solid red'
+  Subscribemail.classList.add('show')
+  
+ } else {
+  Subscribemail.style.border = 'none'
+  Subscribemail.classList.remove('show')
+
+ }
+ // empty form if submitted
+ Subscribemail.value = ''
+ })
+
